5742f5ecfbc00761dc3eef09be8e387f8ac453bc
masiDaniel <masidaniel02@gmail.com>
2025-12-10 23:30:52 +0300

    initial setup - login UI

---
+++ flutter/zoom_clone/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/login_screen.dart';
+import 'package:zoom_clone/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Zoom',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      routes: {'/login': (context) => LoginScreen()},
+      home: LoginScreen(),
+    );
+  }
+}
